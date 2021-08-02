b80d2ed69faa8b5327c4b44ccfc042e3a33a88fb
GuiQRSM <endbeob15@gmail.com>
2021-08-02 13:52:09 -0300

    refactors .max() and .min() by maxOrNull() and minOrNull().

--- src/main/kotlin/idea/collections/TestOperaçoes.kt
+++ src/main/kotlin/idea/collections/TestOperaçoes.kt
@@ -9,8 +9,8 @@ fun main() {
 
     println("------------------------------------------")
 
-    println("Maior salário: ${salarios.max()}")
-    println("Menor salário: ${salarios.min()}")
+    println("Maior salário: ${salarios.maxOrNull()}")
+    println("Menor salário: ${salarios.minOrNull()}")
     println("Media dos salários: ${salarios.average()}")
 
    val exemplefilter = salarios.filter { it > 2000.0 }
