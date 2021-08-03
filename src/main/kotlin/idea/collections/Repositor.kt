ad890418ce8570c0078f8826b542a0d355eb7448
GuiQRSM <endbeob15@gmail.com>
2021-08-03 15:53:22 -0300

    In one class, insert a mutableMapOf, for in another file this mutableMapOf is operated.

---
+++ src/main/kotlin/idea/collections/Repositor.kt
@@ -0,0 +1,20 @@
+package idea.collections
+
+class BC <T> {
+    private val dados = mutableMapOf<String, T>()
+
+    fun Implemnt(id: String, Value: T) {
+        dados[id] = Value
+    }
+
+    fun get(id: String) = dados[id]
+}
+
+
+
+
+
+
+
+
+
---
+++ src/main/kotlin/idea/collections/TestMutableMapOf.kt
@@ -0,0 +1,15 @@
+package idea.collections
+
+fun main() {
+    val erick = Funcionario("Erick", 5000.0, "pj", "Matutino")
+    val afonso = Funcionario("Afonso", 2000.0, "CLT", "Noturno")
+    val nubia = Funcionario("Nubia", 8000.0, "PJ", "Matutino")
+
+   val relatorio = BC<Funcionario>()
+
+    relatorio.Implemnt(erick.nome, erick)
+    relatorio.Implemnt(afonso.nome, afonso)
+    relatorio.Implemnt(nubia.nome, nubia)
+
+    println(relatorio.get(afonso.nome))
+}
